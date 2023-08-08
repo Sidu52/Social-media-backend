7501f6bd0c54ce8287fc5430c8ab2d5806dee975
Siddhant sharma <72178357+Sidu52@users.noreply.github.com>
2023-08-08 14:18:37 +0530

    Update homecontroller.js

--- controller/homecontroller.js
+++ controller/homecontroller.js
@@ -105,7 +105,7 @@ const getPosts = async (req, res) => {
         const localuser = await User.findById(id);//find local User for fillter post according following
 
         //If user not login than show all Post
-        if (!localuser) {
+        if (!localuser || localuser.following.length==0) {
             post = await Post.find();//Find all Post
             return res.status(201).json({ data: post, user: user });
         }
